aa853fecc64d9dfdcfff40be3fb6d72b9cfcf45e
shinji1095 <shinji1095nameko@gmail.com>
2021-04-12 23:41:29 +0900

    add message

--- messages/follow.js
+++ messages/follow.js
@@ -1,4 +1,4 @@
 module.exports = {
     "type": "text",
-    "text": "和田研究室へようこそ！下のURLからユーザー登録してね！https://student-attendance--manager.herokuapp.com//liff/student/register"
+    "text": "和田研究室へようこそ！URLからユーザー登録してね！https://student-attendance--manager.herokuapp.com/liff/student/register"
   }
