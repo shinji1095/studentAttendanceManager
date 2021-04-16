1ef2c2c1eab473faa2da561c4e618f02d734c7e1
shinji1095 <shinji1095nameko@gmail.com>
2021-04-16 23:03:34 +0900

    modify leave

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 const webhookController = require("../controllers/webhookController");
-const DateController = require("../controllers/DateController");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
