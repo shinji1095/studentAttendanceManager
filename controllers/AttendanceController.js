5a54aa525be86a4e03dac5bf974165682f806093
shinji1095 <shinji1095nameko@gmail.com>
2021-04-17 00:38:58 +0900

    modify leave

--- controllers/AttendanceController.js
+++ controllers/AttendanceController.js
@@ -6,15 +6,20 @@ const {Op} = require("sequelize");
 
 module.exports = {
     attend: async (req, res, next) => {
-        const {hashID, roomID, leave} = req.body;
+        let {hashID, roomID, arrival, leave} = req.body;
+        const now = moment().format();
+        (arrival == "now") ?arrival = now :arrival = moment(new Date().setHours(arrival)).format();
+        (leave == "now") ?leave = now :leave = moment(new Date().setHours(leave)).format();
+
         const user = await User.findOne({
             where: {hashID},
-            attributes: ["id"]
         })
         .catch(err => {
             console.log(err);
         })
         console.log(user)
+        console.log("-----------------start of ", moment().startOf());
+        console.log("-------------------end of ", moment().endOf());
         if(user !== null){
             const userID = user.dataValues.id;
             const attendance = await Attendance.findAndCountAll({
@@ -28,35 +33,52 @@ module.exports = {
                     }
                 }
             })
-            
-            if(attendance.count === 0){
-                const now = moment().format();
+
+            const {count, rows} = attendance;
+            if(count == 0){
                 Attendance.create({
                     roomID,
-                    arrival: now,
-                    userID,
-                    leave
+                    arrival,
+                    leave,
+                    userID
                 });
+    
+                GASController.post({
+                    date: moment(now).format("MM-DD"),
+                    arrival: moment(arrival).format("HH:mm"),
+                    leave: moment(leave).format("HH:mm"),
+                    academic: user.dataValues.academic,
+                    studentID: user.dataValues.studentID,
+                    userName: user.dataValues.name,
+                    roomID,
+                    riskForLunch: null,
+                    riskForDinner: null
+                });
+    
                 res.render("attendDone");
             }else{
-                res.render("leaveYet")
+                res.render("leaveYet");
             }
+            
+            
         }else{
-            console.log("regisYet")
             console.log(user)
             res.render("registerYet");
         }
-        
+
     },
     leave: async (req, res, next) => {
-        const {hashID, riskForLunch, riskForDinner} = req.body;
+        let {hashID, riskForLunch, riskForDinner, leave} = req.body;
+        const now = moment().format();
+        (leave == "now")? leave = now: leave = moment(new Date().setHours(leave));
+
         const user = await User.findOne({
             where: {hashID},
         });
         if(user !== null){
             const userID = user.dataValues.id;
     
-            const attendances = await Attendance.findAll({
+            const attendances = await Attendance.findAndCountAll({
                 where: {
                     userID,
                     [Op.and]:{
@@ -72,32 +94,32 @@ module.exports = {
             console.log("-----------------start of ", moment().startOf());
             console.log("-------------------end of ", moment().endOf());
             console.log("--------------------------------attendance----------------------------/n", attendances)
-            const now = moment().format();
-            attendances.map(attendance => {
-                attendance.leave = now;
-                attendance.riskForLunch = riskForLunch;
-                attendance.riskForDinner = riskForDinner;
-                attendance.save();
-
-            })
-            
-            console.log("user: ", user);
-            const data = {
-                date: moment(now).format("MM-DD"),
-                arrival: moment(attendance.rows[0].arrival).format("HH:mm"),
-                leave: moment(now).format("HH:mm"),
-                academic: user.dataValues.academic,
-                studentID: user.dataValues.studentID,
-                userName: user.dataValues.name,
-                roomID: attendance.rows[0].roomID,
-                riskForLunch,
-                riskForDinner
+            const {count, rows} = attendances;
+            if(count !== 1){
+                rows.map(attendance => {
+                    attendance.leave = leave;
+                    attendance.riskForLunch = riskForLunch;
+                    attendance.riskForDinner = riskForDinner;
+                    attendance.save();
+    
+                    // <Object> data
+                    GASController.post({
+                        date: moment(now).format("MM-DD"),
+                        arrival: moment(attendance.arrival).format("HH:mm"),
+                        leave: moment(leave).format("HH:mm"),
+                        academic: user.dataValues.academic,
+                        studentID: user.dataValues.studentID,
+                        userName: user.dataValues.name,
+                        roomID: attendance.roomID,
+                        riskForLunch,
+                        riskForDinner
+                    });
+                })
+                res.render("completeRegister");
+            }else{
+                res.render("spreadSheetError")
             }
-            
-            // <Object> data
-            GASController.post(data);
-            
-            res.render("completeRegister");
+                        
         }else{
             res.render("registerYet");
         }
