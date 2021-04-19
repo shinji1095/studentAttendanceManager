a97dbcf18bf0b86924e71feb29bc4b76496a9758
shinji1095 <shinji1095nameko@gmail.com>
2021-04-19 12:28:32 +0900

    add delete Attendance function

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const webhookController = require("../controllers/webhookController");
+const Attendance = require("../models").Attendance;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -10,6 +11,15 @@ router.get('/', function(req, res, next) {
 router.get("/webhook/", webhookController.handleEvent);
 router.get("/speech/", (req, res, next) => {
   res.render("onsei")
+})
+
+router.get("/delete/:id", (req, res, next) => {
+  let id = req.params.id;
+  Attendance.destroy({
+    where:{
+      id
+    }
+  })
 })
 
 module.exports = router;
